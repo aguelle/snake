4b02703c8d23bf2c5bf3a2eccbc8c4fc7f6c0e3d
Florian Chartier <fwchartier@gmail.com>
2023-09-11 14:54:11 +0200

    moveSnake can't go back now. clean some code with more exhaustiv functions

--- script.js
+++ script.js
@@ -1,35 +1,65 @@
-// create function to move snake
-
+// to listen keyboard on the window
 window.addEventListener('keydown', moveSnake);
 
+// to record snake position
 let positionTop = 0;
 let positionLeft = 0;
 
 // target element we want to move :
-let ballPosition = document.getElementById('ball');
+let snake = document.getElementById('snake');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+// to draw the snake
+function drawSnake() {
+    ctx.drawImage(snake, positionLeft, positionTop, 40, 40);
+}
+
+// to clear the snake
+function clearSnake() {
+    ctx.clearRect(positionLeft, positionTop, 40, 40);
+}
+
+// call function to start with a snake already visible
+drawSnake();
 
 // console.log to see movements, for futures functions
-function moveSnake() {
+let lastMove;
+
+function moveSnake(event) {
+    console.log(lastMove)
     switch (event.key) {
         case 'ArrowDown':
+            if (lastMove == 'ArrowUp') break; 
+            clearSnake();
             positionTop += 50;
             console.log('ArrowDown' + ` Y : ${positionTop} - X : ${positionLeft}`);
-            ballPosition.style.top = positionTop + 'px';
+            drawSnake();
+            lastMove = event.key;
             break;
         case "ArrowUp":
+            if (lastMove == 'ArrowDown') break;
+            clearSnake();
             positionTop -= 50;
             console.log('ArrowUp' + ` Y : ${positionTop} - X : ${positionLeft}`);
-            ballPosition.style.top = positionTop + 'px';
+            drawSnake();
+            lastMove = event.key;
             break;
         case "ArrowLeft":
+            if (lastMove == 'ArrowRight') break;
+            clearSnake();
             positionLeft -= 50;
             console.log('ArrowLeft' + ` Y : ${positionTop} - X : ${positionLeft}`);
-            ballPosition.style.left = positionLeft + 'px';
+            drawSnake();
+            lastMove = event.key;
             break;
         case "ArrowRight":
+            if (lastMove == 'ArrowLeft') break;
+            clearSnake();
             positionLeft += 50;
             console.log('ArrowRight' + ` Y : ${positionTop} - X : ${positionLeft}`);
-            ballPosition.style.left = positionLeft + 'px';
+            drawSnake();
+            lastMove = event.key;
             break;
         default:
             console.log(event.key, event.keyCode);
@@ -37,3 +67,4 @@ function moveSnake() {
     }
     event.preventDefault();
 }
+
